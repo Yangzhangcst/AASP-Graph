5fb68056ecd3bb300608c3dd2cc85825499a3089
Yangzhangcst <32331398+Yangzhangcst@users.noreply.github.com>
2020-04-14 18:01:45 +0800

    Update set_parameters_oversegmentation.m

--- functions/set_parameters_oversegmentation.m
+++ functions/set_parameters_oversegmentation.m
@@ -1,10 +1,9 @@
 function [para_ms, para_gbis] = set_parameters_oversegmentation(img_loc)
 
-%两个互补的分割方法，Mean Shift 和 FH
 % Set parameters for over-segmentation
 % 1. Two complementary segmentation methods, Mean Shift and FH, are used
 % 2. For each method, different parameters are used
-% 3. Use image complexity to guide the selection of FH parameters（图像复杂度指导选择）
+% 3. Use image complexity to guide the selection of FH parameters
 
 %%% Parameters for Mean Shift
 para_ms.hs{1} = 7; para_ms.hr{1} = 7;  para_ms.M{1} = 100;
@@ -13,14 +12,14 @@ para_ms.hs{3} = 7; para_ms.hr{3} = 11; para_ms.M{3} = 100;
 
 para_ms.K = length(para_ms.hs);
 
-%%% Parameters for FH: adaptive superpixels 自适应
-% 1. Idea: include large superpixels for complex images to suppress包括大型超像素，用于复杂图像的压制
-% strong edges within objects强大的内边缘对象
-% 2. Use LAB color variances to measure image complexity用LAB测量图像复杂度的差异
+%%% Parameters for FH: adaptive superpixels
+% 1. Idea: include large superpixels for complex images to suppress
+% strong edges within objects
+% 2. Use LAB color variances to measure image complexity
 
-img = colorspace('Lab<-', imread(img_loc));%颜色空间转换--LAB
+img = colorspace('Lab<-', imread(img_loc));
 [X,Y,Z] = size(img);%Z=3
-cvar = var( reshape(img,[X*Y,Z]) ); clear img;%重新调整矩阵的行数、列数、维数和求均方差,三维上求方差，3个结果
+cvar = var( reshape(img,[X*Y,Z]) ); clear img;%
 
 if sum(cvar) < 500 || sum(cvar(2:end)) < 100
     % gbis para
